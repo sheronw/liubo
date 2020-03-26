2810b516975c5a94331e58164f55ba6cda611d32
SheronW <sheronw1174@gmail.com>
2020-03-26 15:56:31 -0400

    add mock data

---
+++ data/findspot.js
@@ -0,0 +1,49 @@
+const findspot = {
+  type: "FeatureCollection",
+  features: [
+    {
+      type: "Feature",
+      properties: {
+        name: "棋盘名",
+        description: "这是个棋盘",
+        location: "安阳",
+        time: "唐",
+        iconSize: [60, 60]
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 118.95163, lat: 36.58856 }
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "棋盘名",
+        description: "这是个棋盘",
+        location: "安阳",
+        time: "唐",
+        iconSize: [60, 60]
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 111.1743, lat: 32.04977 }
+      }
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "棋盘名",
+        description: "这是个棋盘",
+        location: "安阳",
+        time: "唐",
+        iconSize: [60, 60]
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 109.9699, lat: 22.69357 }
+      }
+    }
+  ]
+};
+
+export { findspot };
