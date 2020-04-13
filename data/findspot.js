861719d3b12997d9762b015821386854d8970bc9
SheronW <sheronw1174@gmail.com>
2020-04-12 22:41:02 -0400

    add the first real data!

--- data/findspot.js
+++ data/findspot.js
@@ -4,46 +4,61 @@ const findspot = {
     {
       type: "Feature",
       properties: {
-        name: "棋盘名",
-        description: "这是个棋盘",
-        location: "安阳",
-        time: "唐",
-        iconSize: [60, 60]
+        name: "长沙马王堆M3",
+        year: -163,
+        time: "西汉文帝前元十二年",
+        owner: "利仓（西汉长沙国丞相、第一代轪侯）之子",
+        material: "木，四角有剪贴鸟形装饰",
+        dimensions: "45 × 45 × 1.2 cm",
+        description:
+          "-棋子-<br>" +
+          "12枚象牙制棋子，6黑6白 (4.2 × 2.2 × 2.3 cm)<br>" +
+          "20枚象牙制棋子，2.9 × 1.8 × 1.0 cm<br>" +
+          "-算筹/骰子-<br>" +
+          "筹码盒，木制 (24.9 × 24.9 × 17cm)<br>" +
+          "30枚象牙筹 (16.4 cm)<br>" +
+          "12枚象牙筹 (22.7 cm)<br>" +
+          "1个18面骰，木制<br>" +
+          "另：谴册内有较为详细的物品名记载",
       },
       geometry: {
         type: "Point",
-        coordinates: { lon: 118.95163, lat: 36.58856 }
-      }
+        coordinates: { lon: 113.03927, lat: 28.19882 },
+      },
     },
     {
       type: "Feature",
       properties: {
-        name: "棋盘名",
-        description: "这是个棋盘",
-        location: "安阳",
-        time: "唐",
-        iconSize: [60, 60]
+        name: "长沙马王堆M3",
+        year: -163,
+        time: "西汉文帝前元十二年",
+        owner: "",
+        material: "",
+        dimensions: "",
+        description: "",
       },
       geometry: {
         type: "Point",
-        coordinates: { lon: 111.1743, lat: 32.04977 }
-      }
+        coordinates: { lon: 111.1743, lat: 32.04977 },
+      },
     },
     {
       type: "Feature",
       properties: {
-        name: "棋盘名",
-        description: "这是个棋盘",
-        location: "安阳",
-        time: "唐",
-        iconSize: [60, 60]
+        name: "长沙马王堆M3",
+        year: -163,
+        time: "西汉文帝前元十二年",
+        owner: "",
+        material: "",
+        dimensions: "",
+        description: "",
       },
       geometry: {
         type: "Point",
-        coordinates: { lon: 109.9699, lat: 22.69357 }
-      }
-    }
-  ]
+        coordinates: { lon: 109.9699, lat: 22.69357 },
+      },
+    },
+  ],
 };
 
 export { findspot };
