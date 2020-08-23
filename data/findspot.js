7eb5cc35d88f6681303818b477f167933b049df9
SheronW <sheronw1174@gmail.com>
2020-08-23 17:07:04 -0400

    95% data done

--- data/findspot.js
+++ data/findspot.js
@@ -10,16 +10,21 @@ const findspot = {
         owner: "利仓（西汉长沙国丞相、第一代轪侯）之子",
         material: "木，四角有剪贴鸟形装饰",
         dimensions: "45 × 45 × 1.2 cm",
-        description:
-          "-棋子-<br>" +
+        descriptionTag: ["棋子", "算筹/骰子"],
+        description: [
           "12枚象牙制棋子，6黑6白 (4.2 × 2.2 × 2.3 cm)<br>" +
-          "20枚象牙制棋子，2.9 × 1.8 × 1.0 cm<br>" +
-          "-算筹/骰子-<br>" +
+            "20枚象牙制棋子，2.9 × 1.8 × 1.0 cm",
           "筹码盒，木制 (24.9 × 24.9 × 17cm)<br>" +
-          "30枚象牙筹 (16.4 cm)<br>" +
-          "12枚象牙筹 (22.7 cm)<br>" +
-          "1个18面骰，木制<br>" +
-          "另：谴册内有较为详细的物品名记载",
+            "30枚象牙筹 (16.4 cm)<br>" +
+            "12枚象牙筹 (22.7 cm)<br>" +
+            "1个18面骰，木制<br>" +
+            "另：谴册内有较为详细的物品名记载",
+        ],
+        ref:
+          "“长沙马王堆二、三号汉墓发掘简报”，《文物》，湖南省博物馆，中国科学院考古研究所<br>" +
+          "《长沙马王堆二, 三号汉墓》vol.1, 2004, 湖南省博物馆, 湖南省文物考古硏究所编著 ; 主编何介钧；文物出版社<br>" +
+          "Xiong Chuan Xin. 1979. Mawangdui sanhao xihanmu chutu de liubo Liubo Unearthed from the Western Han Tomb No.3 of Mawangdui, In: Wenwu Cultural Relics, vol.4: 35-39." +
+          '图片出自<a href="http://61.187.53.122/collection.aspx?id=1390&lang=zh-CN" target="_blank">湖南省博物馆</a>',
       },
       geometry: {
         type: "Point",
@@ -35,12 +40,14 @@ const findspot = {
         owner: "可能是中山成公（M6墓主）某弟",
         material: "青灰石",
         dimensions: "45cm × 40.2 cm × 0.8 cm",
-        description:
-          "-棋子-<br>" +
-          "小方石11颗，白石质，四面经打磨：正方形6颗，1.5 × 1.5 × 1.5 cm，长方形5颗，1.8 × 1.4 × 1 cm<br>" +
-          "水晶石5颗，2.4 × 1.1 × 1.1 cm（虽大小相似但共有两种形状）<br>" +
-          "-编码-<br>" +
+        descriptionTag: ["棋子", "编码"],
+        description: [
+          "小方石11颗，白石质，四面经打磨：<br>&nbsp;&nbsp;正方形6颗，1.5 × 1.5 × 1.5 cm<br>&nbsp;&nbsp;长方形5颗，1.8 × 1.4 × 1 cm<br>" +
+            "水晶石5颗，2.4 × 1.1 × 1.1 cm（虽大小相似但共有两种形状）",
           "M3: 216 & 217",
+        ],
+        ref:
+          "Hebei wenwu yanjiusuo 河北文物研究所. Lingshou city of Zhongshan state during the warring states period: An excavation report 1975-1993. 战国中山国灵寿城: 1975~1993年考古发掘报告. Beijing: Wenwu chubanshe, 2005.",
       },
       geometry: {
         type: "Point",
@@ -57,46 +64,277 @@ const findspot = {
           "11号墓据简牍为一名名为“喜”的低级官员（县御史、令史）<br>据陪葬品推测，13号墓主身份应低于喜",
         material: "木",
         dimensions: "32 × 29 × 2 cm (11号), 38.5 × 35 × 3.3 cm (13号)",
-        description:
-          "-棋子-<br>" +
+        descriptionTag: ["棋子"],
+        description: [
           "11号墓出土12枚骨质棋子，涂有黑漆，6枚长方形（1.4 × 1 × 2.4 cm），6枚方形（1.4 × 1.4 × 2.4 cm）<br>" +
-          "13号墓出土6枚骨质棋子，1枚红漆（3 × 1.4 × 2 cm），5枚黑漆（2.5 × 1.2 × 1.7 cm）",
+            "13号墓出土6枚骨质棋子，1枚红漆（3 × 1.4 × 2 cm），5枚黑漆（2.5 × 1.2 × 1.7 cm）",
+        ],
+        ref: "《云梦睡虎地秦墓》",
       },
       geometry: {
         type: "Point",
-        coordinates: { lon: 113, lat: 31 },
+        coordinates: { lon: 113, lat: 31.05 },
       },
     },
     {
       type: "Feature",
       properties: {
-        name: "长沙马王堆M3",
-        year: -163,
-        time: "西汉文帝前元十二年",
-        owner: "",
-        material: "",
-        dimensions: "",
-        description: "",
+        name: "湖北云梦县大坟头一号西汉墓",
+        year: -200,
+        time: "西汉早期",
+        owner: "未知",
+        material:
+          "木局，<br>正面白底红漆绘规矩纹，四周绘一道红漆；<br>背为素面，四个侧面用红漆绘有几何纹或植物纹图案",
+        dimensions: "38 × 36 × 2 cm",
+        descriptionTag: [],
+        description: [],
+        ref: "湖北云梦西汉墓发掘简报 文物 1973.9",
       },
       geometry: {
         type: "Point",
-        coordinates: { lon: 109.9699, lat: 22.69357 },
+        coordinates: { lon: 113, lat: 30 },
       },
     },
     {
       type: "Feature",
       properties: {
-        name: "长沙马王堆M3",
-        year: -163,
-        time: "西汉文帝前元十二年",
-        owner: "",
-        material: "",
-        dimensions: "",
-        description: "",
+        name: "甘肃天水放马滩战国秦汉墓群",
+        year: -250,
+        time: "Second half of the third century BCE",
+        owner: "未知，一棺一椁，死者年龄35岁左右",
+        material: "木",
+        dimensions: "12.7 x 5.8 x 0.3 cm",
+        descriptionTag: ["编码", "位置"],
+        description: ["M14:9", "在死者的胸口"],
+        ref:
+          "《甘肃天水放马滩战国秦汉墓群的发掘》甘肃文物考古研究所，天水市北道区文化馆",
       },
       geometry: {
         type: "Point",
-        coordinates: { lon: 109.9699, lat: 22.69357 },
+        coordinates: { lon: 106.113435, lat: 34.417605 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "江苏邗江姚庄101号汉墓",
+        year: -204,
+        time: "西汉晚期",
+        owner: "竖穴土坑木椁夫妇合葬墓，男墓主人应为广陵国的中级武官",
+        material: "漆，木胎",
+        dimensions: "28 x 28 x 6.8 cm",
+        descriptionTag: ["编码", "形状", "位置"],
+        description: [
+          "M101:39",
+          "平面呈正方形，中间刻10厘米见方的正方形，中心雕“田”形方孔。<br>" +
+            "正方形周围刻四个“T”形曲道。边框四边各刻一个“L”形曲道。<br>" +
+            "四交角处各刻一个“田”状方孔。盘下置四足。通体髹褐漆，局中心、边框及足等部分均朱绘几何纹和火焰状云气纹，间饰羽人及麒麟、青龙、白虎、朱雀等。",
+          "头箱",
+        ],
+        ref:
+          "Reference: Yangzhou bowuguan The Yangzhou Museum. 1988. Jiangsu hanjiang yaozhuang 101 hao xihanmu Excavation of the Western Han Tomb No.101 at Yaozhuang, in Hanjiang County, Jiangsu Province, In: Wenwu Cultural Relics, vol.2: 19-43.",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 119.44759, lat: 32.44017 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "山东临沂金雀山九座汉代墓葬",
+        year: -228,
+        time: "31 & 34为战国晚期至西汉早期，M33为西汉中期或晚期",
+        owner: "均为一棺一椁，中小地主或低级官吏",
+        material: "木",
+        dimensions: "见下",
+        descriptionTag: ["编码", "形状", "位置"],
+        description: [
+          "M31:27; M33:16; M34 (棋盘无编号，木棋子为M34:14)",
+          "边箱内",
+        ],
+        ref: "《金雀山九座汉代墓葬》",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 118.35125, lat: 35.04636 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "陕西咸阳原汉景帝阳陵",
+        year: -141,
+        time: "公元前141年",
+        owner: "西汉汉景帝刘启及其皇后王氏",
+        material: "陶",
+        dimensions: "边长 34.5 cm",
+        descriptionTag: ["出土地点"],
+        description: [
+          "陵园内的二号建筑遗址，位于帝陵东南约300米处的大型礼制建筑",
+        ],
+        ref: "汉阳陵博物馆，汉阳陵博物馆编，文物出版社，2007年",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 108.9235972, lat: 34.4444454 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "江苏天长县三角圩",
+        year: -130,
+        time: "西汉（晚于早期，但不晚于元帝）",
+        owner: "广陵国时期的渴者属官（桓平）",
+        material: "木胎黑漆",
+        dimensions: "边长 42 cm",
+        descriptionTag: ["编码", "花纹"],
+        description: [
+          "M19:18",
+          "盘面阴刻六博纹,朱绘云纹,每方各绘一壮年男子与一老翁对博。人像头戴无峨冠,身着红花深衣或着无花深衣,双袖合拢,席地而坐,作对博状。盘两侧各有一长方形抽屉,内装盘边长42厘米。",
+        ],
+        ref:
+          "Anhui sheng wenwu kaogu yanjiusuo Institute of Cultural Relics of Anhui Province (et al.). 1993. Anhui Tianchang xian Sanjiaoxu zhanguo xihanmu chutu wenwu The Cultural Relics Unearthed from the Tombs of the Warring States Period to Western Han Dynasty at Sanjiaoxei in Tianchang County, Anhui, In: Wenwu Cultural Relics, vol.9: 1-31.",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 119.045346, lat: 32.716481 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "湖北荆州天星观二号墓",
+        year: -333,
+        time: "战国中期",
+        owner:
+          "初步判断M2的墓主为女性, 极有可能是M1的夫人, 即M1墓主邸阳君番乘夫人的墓",
+        material: "漆木",
+        dimensions: "28.2 × 45.4 × 40.4 cm",
+        descriptionTag: ["编码", "棋子", "形状"],
+        description: [
+          "M2:D35",
+          "出土时发现有棋子18颗",
+          "盘面平整，有骨角质棋格数道嵌人盘面。平底, 有两个长条形凹槽, 各置一块木板以承足, 足上部为圆弧形较粗, 下部较细, 二足完整, 一足残, 一足缺失。",
+        ],
+        ref:
+          "Hubei sheng jingzhou bowuguan The Jingzhou Museum, Hubei. 2003. Jingzhou tianxingguan er hao chumu The Chu State Tomb No.2 at Tianxingguan of Jingzhou. Peking: Wenwu chubanshe Cultural Relics Publishing House.",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 112.4359912, lat: 30.4047858 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "湖南长沙望城坡古坟垸渔阳墓",
+        year: -157,
+        time: "西汉（文帝~景帝），出土陶器及漆木器与马王堆一、三号墓形制类似",
+        owner:
+          "由出土木楬推测墓主应为汉初吴氏长沙国的某一代王后。<br>" +
+          "墓主可能是汉皇室的某位公主，由于政治等原因而适长沙王，”渔阳“乃墓主所食封邑之称。",
+        material: "漆木",
+        dimensions: "43.2 x 43.2 x 4 cm (C:108)",
+        descriptionTag: ["编码", "位置", "漆骰子（茕）", "博局盘"],
+        description: [
+          "C:105/107/108",
+          "东藏室（C区）第二层器物",
+          "编码 C:34/104。球形十八面体，木胎。通体髹黑漆，阴刻篆文，内填朱漆。<br>" +
+            "每面分别阴刻“一”至“十六”字，剩余两面的一面刻“酒来”，相对一面刻" +
+            "\u{31146}" +
+            "字。直径5.2厘米。",
+          "2件，形制略同。方形。中央方框，四周有曲道，皆刻浅槽。底座与局盘用竹钉连接。通体髹黑漆，曲尺形底座装饰云气纹。",
+        ],
+        ref: "湖南长沙望城坡西汉渔阳墓发掘简报",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 112.9179262, lat: 28.2076373 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "江苏仪征张集团山西汉墓",
+        year: -125,
+        time:
+          "西汉早期，不晚于西汉武帝元狩五年<br>" +
+          "如可以确认其为江都王的陪葬墓公元前153年,下限应为公元前127年或稍后",
+        owner: "女性，团山四墓中地位最高。可能是江都王刘非的陪葬墓",
+        material: "木胎髹褐漆",
+        dimensions: "41,5 x 41.5 x 4.5 cm",
+        descriptionTag: ["编码", "位置", "形状", "木筹"],
+        description: [
+          "1:96",
+          "头箱，与磺形器、环形器、铃、樵益钮等置放在一起，用途不明",
+          "盘面用双线阴刻成六博纹 , 盘边缘下部内收 , 两侧各有一抽屉 , 内各装木筹六根",
+          "木筹为细圆柱状，髹褐漆。长 16.2 cm、直径 0.6 cm",
+        ],
+        ref:
+          "Nanjing bowuyuan The Nanjing Museum (et al.). 1992. Yizheng zhangji tuanshan xihanmu Western Han Tombs at Tuanshan, Zhangji, Yizheng, In: Kaogu xuebao Acta Archaeological Sinica, vol.4: 477-507.",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 119.29592, lat: 32.389789 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "广西西林县普驮铜鼓墓葬",
+        year: -204,
+        time: "西汉早期",
+        owner:
+          "男，25岁左右。社会地位及身份无文字考，据推测当是句町县统治阶级的一员",
+        material: "铜",
+        dimensions: "30 x 29 x 9 cm",
+        descriptionTag: ["形状"],
+        description: [
+          "一件，似案。面方形，折边，下有四只高足。盘面有清晰的棋局。",
+        ],
+        ref: "暂无",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 105.0939, lat: 24.48968 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "徐州北洞山西汉楚王陪葬墓",
+        year: -122,
+        time: "",
+        owner: "",
+        material: "",
+        dimensions: "",
+        descriptionTag: [""],
+        description: [""],
+        ref: "",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 113.267393, lat: 23.143952 },
+      },
+    },
+    {
+      type: "Feature",
+      properties: {
+        name: "西汉南越王墓 (广州象岗山)",
+        year: -122,
+        time: "约公元前122年",
+        owner: "第二代南越王赵昧，赵佗之孙，号称文帝，公元前137年至122年在位",
+        material: "木",
+        dimensions: "",
+        descriptionTag: [""],
+        description: [""],
+        ref: "",
+      },
+      geometry: {
+        type: "Point",
+        coordinates: { lon: 113.267393, lat: 23.143952 },
       },
     },
   ],
